b2444cba09ead1575f57a88d33d784cf1f0ec301
vreztsov <vreztsov90@mail.ru>
2024-01-22 14:52:21 +0300

    Add binding and formatted counters

---
+++ app/src/main/java/ru/netology/nmedia/util/NMediaUtils.kt
@@ -0,0 +1,22 @@
+package ru.netology.nmedia.util
+
+object NMediaUtils {
+    private const val letters: String = "KM"
+
+    fun numFormat(num: Int): String {
+        val numAsString = num.toString()
+        val index = (numAsString.length - 1) / 3 - 1
+        when {
+            index < 0 -> return numAsString
+            index >= letters.length -> throw IllegalArgumentException("Number too large")
+        }
+        val letter = letters[index]
+        val pos = numAsString.length - 3 * (index + 1)
+        val begin = numAsString.substring(0, pos)
+        val decimal = when {
+            begin.length > 1 || numAsString[pos] == '0' -> ""
+            else -> "." + numAsString[pos]
+        }
+        return begin + decimal + letter
+    }
+}
